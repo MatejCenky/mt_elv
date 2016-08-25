ba4e17f0a54609aab862b2e7060181eef9299ae0
Mattto <Mattto@Mattto_Silver>
2016-08-25 19:33:42 +0200

    overload / vibration class edited

--- MT_ELV/src/mt_main/overload.java
+++ MT_ELV/src/mt_main/overload.java
@@ -21,10 +21,10 @@ public class overload {
     public overload(double spans[], double heights[]){
         state_equation.a = spans;
         state_equation.dh = heights;
-        this.ro = 1.25;
-        this.RR = 0;
-        this.k_p = 3;
-        this.C_cl = 1.1;
+        overload.ro = 1.25;
+        overload.RR = 0;
+        overload.k_p = 3;
+        overload.C_cl = 1.1;
     }
     
     /**
@@ -33,69 +33,69 @@ public class overload {
     public overload(){
         state_equation.a[0] = -1;
         state_equation.dh[0] = -1;
-        this.ro = 1.25;
-        this.RR = 0;
-        this.k_p = 3;
-        this.C_cl = 1.1;
+        overload.ro = 1.25;
+        overload.RR = 0;
+        overload.k_p = 3;
+        overload.C_cl = 1.1;
     }
    
  /* Defining variables */
     
 // towers and conductors
-    private double d;           // conductor diameter [m]
-    private double g_c;         // specific weight of conductor [N/m]
+    private static double d;           // conductor diameter [m]
+    private static double g_c;         // specific weight of conductor [N/m]
     
 // type of ice
-    private double ro_I;        // density of the ice (= 500 kg/m3)
-    private final double C_cl; // aerodynamic resistance for conductor with ice coefficient (= 1,1) [-]
+    private static double ro_I;        // density of the ice (= 500 kg/m3)
+    private static double C_cl; // aerodynamic resistance for conductor with ice coefficient (= 1,1) [-]
     
 // icing area
-    private double K_lc;        // local conditions coefficient (čl.4.5.1/SK.3) [-]
-    private double K_h;         // the height coefficient čl.4.5.1/SK.3) [-]
-    private double I_R50;       // reference ice load -> chosen from local icing area (čl.4.5.1/SK.3)
+    private static double K_lc;        // local conditions coefficient (čl.4.5.1/SK.3) [-]
+    private static double K_h;         // the height coefficient čl.4.5.1/SK.3) [-]
+    private static double I_R50;       // reference ice load -> chosen from local icing area (čl.4.5.1/SK.3)
     
 // terrain and wind area
-    private double k_r;         // terrain coefficient (čl. 4.3.2) [-]
-    private double z_0;         // length of the roughness (čl. 4.3.2) [-]
-    private double V_b0;        // base wind speed (čl. 4.3.1/SK.1) [m/s]
-    private double c_dir;       // wind direction coefficient [-]
-    private double c_0;         // orography coefficient [-]
-    private double C_c;         // aerodynamic resistance of the conductor coefficient (čl. 4.4.1.3/SK.1) [-]
+    private static double k_r;         // terrain coefficient (čl. 4.3.2) [-]
+    private static double z_0;         // length of the roughness (čl. 4.3.2) [-]
+    private static double V_b0;        // base wind speed (čl. 4.3.1/SK.1) [m/s]
+    private static double c_dir;       // wind direction coefficient [-]
+    private static double c_0;         // orography coefficient [-]
+    private static double C_c;         // aerodynamic resistance of the conductor coefficient (čl. 4.4.1.3/SK.1) [-]
     
 // directive constants -> defined as final -> set in constructor
-    private final double k_p;   // tip coefficient (= 3) (čl. 4.4.1.2)
-    private final double RR;    // resonance response coefficient (RR = R^2 = 0) (čl. 4.4.12)
-    private final double ro;    // density of the wind (= 1.25)
+    private static double k_p;   // tip coefficient (= 3) (čl. 4.4.1.2)
+    private static double RR;    // resonance response coefficient (RR = R^2 = 0) (čl. 4.4.12)
+    private static double ro;    // density of the wind (= 1.25)
 
 // reliability coefficient
-    private double gama_w;      // partial wind load coefficient [-]
-    private double gama_I;      // partial ice load coefficient [-]
-    private double Psi_w;       // combination coefficient (čl. 4.6.6/SK/CZ) [-]
-    private double Psi_I;       // combination ice load coefficient [-]
-    private double B_I;         // combination coefficient (čl. 4.6.6/SK/CZ) [-]
+    private static double gama_w;      // partial wind load coefficient [-]
+    private static double gama_I;      // partial ice load coefficient [-]
+    private static double Psi_w;       // combination coefficient (čl. 4.6.6/SK/CZ) [-]
+    private static double Psi_I;       // combination ice load coefficient [-]
+    private static double B_I;         // combination coefficient (čl. 4.6.6/SK/CZ) [-]
     
 // computed values
-    private double h_c_mean;    // mean height of the conductor above the ground [m]     
-    private double V_h;         // the mean wind speed [m/s]
-    private double I_v;         // turbulence intensity
-    private double L_m;         // length for response origin coefficient [m]
-    private double L;           // specific length of turbulence [m]
-    private double BB;          // response origin coefficient [m] (= B^2)
-    private double G_c;         // construction coefficient [-]
+    private static double h_c_mean;    // mean height of the conductor above the ground [m]     
+    private static double V_h;         // the mean wind speed [m/s]
+    private static double I_v;         // turbulence intensity
+    private static double L_m;         // length for response origin coefficient [m]
+    private static double L;           // specific length of turbulence [m]
+    private static double BB;          // response origin coefficient [m] (= B^2)
+    private static double G_c;         // construction coefficient [-]
     
-    private double q_wc;        // characteristic wind load
-    private double q_p;         // peak wind pressure
-    private double q_h;         // mean wind pressure
-    private double I_50;        // characteristic ice load
+    private static double q_wc;        // characteristic wind load
+    private static double q_p;         // peak wind pressure
+    private static double q_h;         // mean wind pressure
+    private static double I_50;        // characteristic ice load
     
-    private double I_T;         // extreme ice load
-    private double q_wT;        // extreme wind load
-    private double I_3;         // mild ice load
-    private double q_wI3;       // combined load for mild wind with extreme ice
-    private double q_wIT;       // combined load for extreme wind with mild ice
+    private static double I_T;         // extreme ice load
+    private static double q_wT;        // extreme wind load
+    private static double I_3;         // mild ice load
+    private static double q_wI3;       // combined load for mild wind with extreme ice
+    private static double q_wIT;       // combined load for extreme wind with mild ice
     
-    private double D_I;         // equivalent diameter for conductor with extreme ice [m]
-    private double D_i;         // equivalent diameter for conductor with mild ice [m]
+    private static double D_I;         // equivalent diameter for conductor with extreme ice [m]
+    private static double D_i;         // equivalent diameter for conductor with mild ice [m]
     
 // results
     /**
@@ -121,27 +121,27 @@ public class overload {
 // **************** PUBLIC METHODS **************** //
     public void set_variables(){
         // local - results
-        this.h_c_mean = -1;
-        this.V_h = -1;
-        this.I_v = -1;
-        this.L_m = -1;
-        this.L = -1;
-        this.BB = -1;
-        this.G_c = -1;
+        overload.h_c_mean = -1;
+        overload.V_h = -1;
+        overload.I_v = -1;
+        overload.L_m = -1;
+        overload.L = -1;
+        overload.BB = -1;
+        overload.G_c = -1;
         
-        this.q_wc = -1;
-        this.q_p = -1;
-        this.q_h = -1;
-        this.I_50 = -1;
+        overload.q_wc = -1;
+        overload.q_p = -1;
+        overload.q_h = -1;
+        overload.I_50 = -1;
         
-        this.I_T = -1;
-        this.q_wT = -1;
-        this.I_3 = -1;
-        this.q_wI3 = -1;
-        this.q_wIT = -1;
+        overload.I_T = -1;
+        overload.q_wT = -1;
+        overload.I_3 = -1;
+        overload.q_wI3 = -1;
+        overload.q_wIT = -1;
         
-        this.D_I = -1;
-        this.D_i = -1;
+        overload.D_I = -1;
+        overload.D_i = -1;
         
         // public - results
         overload.z_1 = -1;
@@ -159,7 +159,7 @@ public class overload {
     
     public void compute(){
         //basic check if variables are set
-        if (this.k_r == -1) {
+        if (overload.k_r == -1) {
             set_variables();
             System.out.println("Variables were set!");
         } 
@@ -194,7 +194,7 @@ public class overload {
      * computes the height coefficient "K_h" (čl.4.5.1/SK.3) [-]
      */
     private void height_coefficient(){
-        this.K_h = Math.pow(this.h_c_mean/10,0.13);
+        overload.K_h = Math.pow(overload.h_c_mean/10,0.13);
     }
     
     /**
@@ -208,21 +208,21 @@ public class overload {
         for (i=0; i<state_equation.dh.length; i++){ //[n+1]
             aux += state_equation.dh[i];
         }
-        this.h_c_mean = aux/state_equation.a.length; // [n]
+        overload.h_c_mean = aux/state_equation.a.length; // [n]
     }
     
     /**
      * Computes the mean wind speed "V_h" using the values chosen from the user (from mainframe)
      */
     private void mean_wind_speed() {
-        this.V_h = this.V_b0*this.c_dir*this.c_0*this.k_r*Math.log(this.h_c_mean/this.z_0);
+        overload.V_h = overload.V_b0*overload.c_dir*overload.c_0*overload.k_r*Math.log(overload.h_c_mean/overload.z_0);
     }
     
     /**
      * Computes the turbulence intensity "I_v"
      */
     private void turbulence_intensity(){
-        this.I_v = 1 / (this.c_0*Math.log(this.h_c_mean/this.z_0));
+        overload.I_v = 1 / (overload.c_0*Math.log(overload.h_c_mean/overload.z_0));
     }
     
     /**
@@ -237,77 +237,77 @@ public class overload {
         if (aux > 3000){
             aux = 3000;
         }
-        this.L_m = aux;
+        overload.L_m = aux;
     }
     
     /**
      * Computes the specific length for turbulence "L" [m]
      */
     private void specific_turbulence_length(){
-        this.L = 300 * Math.pow(this.h_c_mean/200, 0.67 + 0.05*Math.log(this.z_0));
+        overload.L = 300 * Math.pow(overload.h_c_mean/200, 0.67 + 0.05*Math.log(overload.z_0));
     }
     
     /**
      * Computes the response origin coefficient "BB = B^2" [m]
      */
     private void response_coefficient (){
-        this.BB = 1 / (1 + 3/2 * this.L_m/this.L);
+        overload.BB = 1 / (1 + 3/2 * overload.L_m/overload.L);
     }
     
     /**
      * Computes the construction coefficient "G_c"
      */
     private void construcion_coefficient(){
-        this.G_c = (1 + 2*this.k_p*this.I_v*Math.sqrt(this.BB + this.RR))/(1 + 7*this.I_v);
+        overload.G_c = (1 + 2*overload.k_p*overload.I_v*Math.sqrt(overload.BB + overload.RR))/(1 + 7*overload.I_v);
     }
     
     /**
      * Computes characteristic wind load "q_wc"
      */
     private void characteristic_wind_load(){
-        this.q_wc = this.q_p* this.d* this.C_c* this.G_c;
+        overload.q_wc = overload.q_p* overload.d* overload.C_c* overload.G_c;
     }
     
     /**
      * Computes the peak wind pressure "q_p"
      */
     private void peak_wind_pressure(){
-        this.q_p = (1 + 7* this.I_v)* this.q_h;
+        overload.q_p = (1 + 7* overload.I_v)* overload.q_h;
     }
     
     /**
      * Computes the mean wind pressure "q_h"
      */
     private void mean_wind_pressure(){
-        this.q_h = 1/2 * this.ro * Math.pow(this.V_h, 2);
+        overload.q_h = 1/2 * overload.ro * Math.pow(overload.V_h, 2);
     }
     
     /**
      * Computes the characteristic ice load "I_50"
      */
     private void characteristic_ice_load(){
-        this.I_50 = this.I_R50* this.K_h* this.K_lc;
+        overload.I_50 = overload.I_R50* overload.K_h* overload.K_lc;
     }
     
     /**
      * Computes the extreme ice load "I_T"
      */
     private void extreme_ice_load(){
-        this.I_T = this.I_50* this.gama_I;
+        overload.I_T = overload.I_50* overload.gama_I;
     }
     
     /**
      * Computes the extreme wind load "q_wT"
      */
     private void extreme_wind_load(){
-        this.q_wT = this.q_wc* this.gama_w;
+        overload.q_wT = overload.q_wc* overload.gama_w;
     }
     
     /**
      * Computes the mild ice load "I_3"
      */
     private void mild_ice_load(){
-        this.I_3 = this.I_50* this.Psi_I;
+        overload.I_3 = overload.I_50* overload.Psi_I;
     }
     
     /**
@@ -316,8 +316,8 @@ public class overload {
      * - mild ice "D_i"
      */
     private void equivalent_diameter_with_ice(){
-        this.D_I = Math.sqrt(this.d* this.d + (4*this.I_T)/(9.80665*Math.PI* this.ro_I));   // extreme
-        this.D_i = Math.sqrt(this.d* this.d + (4*this.I_3)/(9.80665*Math.PI* this.ro_I));   // mild
+        overload.D_I = Math.sqrt(overload.d* overload.d + (4*overload.I_T)/(9.80665*Math.PI* overload.ro_I));   // extreme
+        overload.D_i = Math.sqrt(overload.d* overload.d + (4*overload.I_3)/(9.80665*Math.PI* overload.ro_I));   // mild
     }
     
     /**
@@ -326,15 +326,15 @@ public class overload {
      * - extreme wind with mild ice "q_wIT"
      */
     private void combined_load(){
-        this.q_wI3 = this.q_p* this.D_I* this.C_cl* this.G_c* this.Psi_w;
-        this.q_wIT = this.q_p* this.D_i* this.C_cl* this.G_c* this.B_I* this.B_I;
+        overload.q_wI3 = overload.q_p* overload.D_I* overload.C_cl* overload.G_c* overload.Psi_w;
+        overload.q_wIT = overload.q_p* overload.D_i* overload.C_cl* overload.G_c* overload.B_I* overload.B_I;
     }
     
     /**
      * Computes extreme ice overload on the conductor "z_1"
      */
     private void overload_extreme_ice(){
-        overload.z_1 = (this.I_T + this.g_c) / this.g_c;
+        overload.z_1 = (overload.I_T + overload.g_c) / overload.g_c;
     }
     
     /**
@@ -343,14 +343,14 @@ public class overload {
      * - extreme wind with mild ice "z_wI"
      */
     private void overload_combined(){
-        overload.z_Iw = Math.sqrt((Math.pow(this.I_T + this.g_c,2) + this.q_wI3*this.q_wI3)) / this.g_c;
-        overload.z_wI = Math.sqrt((Math.pow(this.I_3 + this.g_c,2) + this.q_wIT*this.q_wIT)) / this.g_c;
+        overload.z_Iw = Math.sqrt((Math.pow(overload.I_T + overload.g_c,2) + overload.q_wI3*overload.q_wI3)) / overload.g_c;
+        overload.z_wI = Math.sqrt((Math.pow(overload.I_3 + overload.g_c,2) + overload.q_wIT*overload.q_wIT)) / overload.g_c;
     }
     
     /**
      * Computes extreme wind overload in conductor "z_W"
      */
     private void overload_extreme_wind(){
-        overload.z_w = Math.sqrt((this.q_wT* this.q_wT + this.g_c* this.g_c)) / this.g_c;
+        overload.z_w = Math.sqrt((overload.q_wT* overload.q_wT + overload.g_c* overload.g_c)) / overload.g_c;
     }
 }
--- MT_ELV/src/mt_main/vibration_protection.java
+++ MT_ELV/src/mt_main/vibration_protection.java
@@ -22,5 +22,28 @@ package mt_main;
  */
 public class vibration_protection {
     
+    /**
+     * Class is used for computation of the vibration protection
+     * - it is checking the safety for the spans
+     * @param terrain [1-4] - define the terrain
+     *      1 - open flat terrain, without trees, with snow, near / over water areas
+     *      2 - open flat terrain, without trees, without snow
+     *      3 - open flat / rough terrain, few trees, occasional breaks / barriers
+     *      4 - built-up terrain with trees and buildings, forests, fields with bushes
+     */
+    public vibration_protection(int terrain){
+        switch(terrain){
+            case 1:
+                vibration_protection.c_vib = 1000;
+            case 2:
+                vibration_protection.c_vib = 1125;
+            case 3:
+                vibration_protection.c_vib = 1225;
+            case 4:
+                vibration_protection.c_vib = 1425;
+        }
+    }
+
+    private static double c_vib;
     
 }
